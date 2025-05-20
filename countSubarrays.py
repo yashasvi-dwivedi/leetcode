44356fecb3b43ea23c303ffa6260ddbf31698a99
yashasvi-dwivedi <randomdwivedi@gmail.com>
2025-05-20 10:28:13 +0530

    cSubarray

---
+++ countSubarrays.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def countSubarrays(self, nums, k):
+        m = max(nums)
+        m_f = i = c = 0
+        for j in range(len(nums)):
+            if m == nums[j]:
+                m_f += 1
+            while m_f >= k:
+                c += len(nums) - j
+                if nums[i] == m:
+                    m_f -= 1
+                i += 1
+        return c
+
+
+# Example usage:
+if __name__ == "__main__":
+    nums = [1, 2, 3, 4, 5]
+    k = 2
+    solution = Solution()
+    result = solution.countSubarrays(nums, k)
+    print(result)
+
+# Output: 10
