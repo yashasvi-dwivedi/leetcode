a3668d289fd28af939713e224e9e51c723cd9834
yashasvi-dwivedi <randomdwivedi@gmail.com>
2025-05-21 18:10:20 +0530

    lilchange

--- threesum.py
+++ threesum.py
@@ -10,19 +10,19 @@ class Solution(object):
             if i > 0 and a == nums[i - 1]:
                 continue
 
-            l, r = i + 1, len(nums) - 1
-            while l < r:
-                threeSum = a + nums[l] + nums[r]
+            left, r = i + 1, len(nums) - 1
+            while left < r:
+                threeSum = a + nums[left] + nums[r]
                 if threeSum > 0:
                     r -= 1
                 elif threeSum < 0:
-                    l += 1
+                    left += 1
                 else:
-                    res.append([a, nums[l], nums[r]])
-                    l += 1
+                    res.append([a, nums[left], nums[r]])
+                    left += 1
                     r -= 1
-                    while l < r and nums[l] == nums[l - 1]:
-                        l += 1
+                    while left < r and nums[left] == nums[left - 1]:
+                        left += 1  # noqa: E741
 
         return res
 
