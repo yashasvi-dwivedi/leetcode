95564062f50d98f08ec2e6f3c059667011803cad
yashasvi-dwivedi <randomdwivedi@gmail.com>
2025-05-27 09:41:19 +0530

    comments

--- differenceOfSums.py
+++ differenceOfSums.py
@@ -1,22 +1,25 @@
 class Solution(object):
     def differenceOfSums(self, n, m):
-        totalSum = n * (n + 1) // 2
-        divsibleSum = m * (n // m) * (n // m + 1)
+        totalSum = n * (n + 1) // 2  # Applying formula to sum (1, n)
+        divsibleSum = (
+            m * (n // m) * (n // m + 1)
+        )  # Formula to get the sum of all divisible numbers
         return totalSum - divsibleSum
 
 
+# Test Cases
 if __name__ == "__main__":
     n = 10
     m = 3
     s = Solution()
     result = s.differenceOfSums(n, m)
-    print(result)
+    print(result)  # Output : 19
     n = 5
     m = 6
     s = Solution()
     result = s.differenceOfSums(n, m)
-    print(result)
+    print(result)  # Output: 15
     n = 5
     m = 1
     result = s.differenceOfSums(n, m)
-    print(result)
+    print(result)  # Output: -15
