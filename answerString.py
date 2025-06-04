525023be38505f3c97400da3d544eb303af92c64
yashasvi-dwivedi <randomdwivedi@gmail.com>
2025-06-04 22:03:24 +0530

    comments

--- answerString.py
+++ answerString.py
@@ -13,3 +13,14 @@ class Soltuion(object):
 
 
 # Test Cases
+if __name__ == "__main__":
+    solution = Soltuion()
+    print(solution.answerString("abacaba", 3))  # Expected: "abac"
+    print(solution.answerString("abcde", 2))  # Expected: "cde"
+    print(solution.answerString("aaaaa", 1))  # Expected: "aaaaa"
+    print(
+        solution.answerString("xyz", 4)
+    )  # Expected: "xyz" (since numFriends > len(word))
+    print(
+        solution.answerString("hello", 5)
+    )  # Expected: "hello" (since numFriends == len(word))
