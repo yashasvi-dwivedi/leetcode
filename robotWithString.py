30a348d56654d65032901eb1dfeb17750cc0890b
yashasvi-dwivedi <randomdwivedi@gmail.com>
2025-06-06 09:28:25 +0530

    comments

--- robotWithString.py
+++ robotWithString.py
@@ -3,27 +3,31 @@ from collections import Counter
 
 class Solution(object):
     def robotWithString(self, s):
-        freq = Counter(s)
-        st = []
-        res = []
+        freq = Counter(s)  # Count the frequency of each character in the string
+        st = []  # Stack to simulate the robot's holding area
+        res = []  # Result list to build the output string
 
+        # Helper function to find the smallest character still available in freq
         def min_char(freq):
-            for i in range(26):
+            for i in range(26):  # Loop through 'a' to 'z'
                 ch = chr(ord("a") + i)
                 if freq[ch] > 0:
-                    return ch
-            return "a"
+                    return ch  # Return the smallest available character
+            return "a"  # Default return (should not be reached)
 
+        # Process each character in the input string
         for ch in s:
-            st.append(ch)
-            freq[ch] -= 1
+            st.append(ch)  # Push the character onto the stack
+            freq[ch] -= 1  # Decrement its frequency
+            # While the top of the stack is <= the smallest remaining character, pop to result
             while st and st[-1] <= min_char(freq):
                 res.append(st.pop())
 
+        # Pop any remaining characters from the stack to the result
         while st:
             res.append(st.pop())
 
-        return "".join(res)
+        return "".join(res)  # Join the result list into a string
 
 
 # Test Cases
