27638619e06c397ad85f86465eec998ff0d1ee03
yashasvi-dwivedi <randomdwivedi@gmail.com>
2025-06-20 14:25:07 +0530

    daily

---
+++ maxDistance.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def maxDistance(self, s, k):
+        res = 0
+        for dirr in "NE", "SE", "SW", "NW":
+            kk, dist = k, 0
+            for c in s:
+                dist += c in dirr or kk > 0 or -1
+                kk -= c not in dirr
+                res = max(res, dist)
+
+        return res
+
+
+# Example usage:
+if __name__ == "__main__":
+    sol = Solution()
+    s = "NSEW"
+    k = 2
+    print(sol.maxDistance(s, k))  # Output: 4
