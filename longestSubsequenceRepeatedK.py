8c61f7ad591eef315881694a34d366b28e96d39d
yashasvi-dwivedi <randomdwivedi@gmail.com>
2025-06-27 18:18:13 +0530

    daily

---
+++ longestSubsequenceRepeatedK.py
@@ -0,0 +1,34 @@
+from collections import deque
+
+
+class Solution:
+    def longestSubsequenceRepeatedK(self, s: str, k: int) -> str:
+        def isK(sub: str, t: str, k: int) -> bool:
+            count = i = 0
+            for ch in t:
+                if i < len(sub) and ch == sub[i]:
+                    i += 1
+                    if i == len(sub):
+                        i = 0
+                        count += 1
+                        if count == k:
+                            return True
+            return False
+
+        res = ""
+        q = deque([""])
+        while q:
+            curr = q.popleft()
+            for ch in map(chr, range(ord("a"), ord("z") + 1)):
+                nxt = curr + ch
+                if isK(nxt, s, k):
+                    res = nxt
+                    q.append(nxt)
+        return res
+
+
+# Test cases
+if __name__ == "__main__":
+    sol = Solution()
+    print(sol.longestSubsequenceRepeatedK("abcabcabc", 2))  # Output: "abc"
+    print(sol.longestSubsequenceRepeatedK("abacabadab", 3))  # Output: "ab"
