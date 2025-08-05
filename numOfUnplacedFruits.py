6222f29952facf0c59aec10d0f9e095848fa02ef
yashasvi-dwivedi <randomdwivedi@gmail.com>
2025-08-05 14:12:30 +0530

    daily

---
+++ numOfUnplacedFruits.py
@@ -0,0 +1,22 @@
+from typing import List
+
+
+class Solution:
+    def numOfUnplacedFruits(self, fruits: List[int], baskets: List[int]) -> int:
+        n = len(fruits)
+        alloted = 0
+
+        for i in range(n):
+            for j in range(n):
+                if fruits[i] <= baskets[j]:
+                    alloted += 1
+                    baskets[j] = -1
+                    break
+
+        return n - alloted
+
+
+# Test cases
+if __name__ == "__main__":
+    s = Solution()
+    print(f"s.numOfUnplacedFruits ")
